2df18a8503e7a7e673a29974e77568b171088917
Arvis Abromovics <info@vsmedio.lv>
2018-01-09 23:34:46 +0100

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,47 @@
+/* 
+И снова Пушистик…
+Добавь один метод в класс Cat так, чтобы программа ничего не выводила на экран.
+
+
+Требования:
+1. Класс Solution должен содержать класс Pet.
+2. Класс Solution должен содержать класс Cat.
+3. Класс Cat должен наследоваться от класса Pet.
+4. Класс Pet должен содержать два метода getName() и setName().
+5. Класс Cat должен переопределять метод setName().
+6. Метод setName() класса Cat должен присваивать пустое значение переменной name.
+7. Программа должна выводить на экран результат метода getName() объекта pet.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Pet pet = new Cat();
+        pet.setName("Я - пушистик");
+
+        System.out.println(pet.getName());
+    }
+
+    public static class Pet {
+        protected String name;
+
+        public Pet() {
+        }
+
+        public final String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+    }
+
+    public static class Cat extends Pet {
+        @Override
+        public void setName(String name) {
+            this.name = "";
+        }
+
+    }
+}
